5f0fa09808e6c05147f407b1cb1768769cc126ff
Keeley Hammond <keeleymhammond@gmail.com>
2017-02-21 15:49:30 -0800

    Added CSS and design, formatted table

--- app.js
+++ app.js
@@ -73,13 +73,8 @@ function generateData() {
   }
 }
 
-//run functions
-createAllImages();
-generateData();
-
 // write random generate function, pulling imgNum id per objects (Amy helped me with this)
 function generateRandom() {
-
   // generate three random numbers
   var imgNumArray = [];
   for (var i = 0; i < allImages.length; i++) {
@@ -87,7 +82,6 @@ function generateRandom() {
       imgNumArray.push(i);
     }
   }
-
   var randArray = [];
   for (i = 0; i < 3; i++) {
     var randIndex = Math.random() * imgNumArray.length;
@@ -116,7 +110,9 @@ function generateRandom() {
   }
 };
 
-// run generate random initially
+// run all functions initially
+createAllImages();
+generateData();
 generateRandom();
 
 // Chart.js
@@ -135,10 +131,34 @@ function createChart() {
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
           'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
