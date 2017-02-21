d5e4c4ffd241a75127089400e5b0720612082232
Keeley Hammond <keeleymhammond@gmail.com>
2017-02-21 15:11:12 -0800

    Refactored object creation, got chart working minus instantly showing new clicks

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 'use strict';
 
+//create array of all filepaths
+var filepathArray = [
+  'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg',
+  'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg',
+  'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
+  'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg',
+  'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg',
+];
 var allImages = [];
 var previousRandArray = [];
+var allNames = [];
+var allClicks = [];
 
 // event listeners per table cell
 var tdOne = document.getElementById('image-one');
@@ -15,7 +25,7 @@ tdThree.addEventListener('click', clickHandler);
 
 // event handler: generate three images to table
 function clickHandler(event) {
-  event.preventDefault();
+  //event.preventDefault();
   // clear table contents
   tdOne.textContent = '';
   tdTwo.textContent = '';
@@ -25,89 +35,49 @@ function clickHandler(event) {
   var clickedImage = event.target.getAttribute('src');
   console.log('Just Clicked:', clickedImage);
   generateRandom();
+  generateData();
   for (var i = 0; i < allImages.length; i++) {
     if (allImages[i].filepath == clickedImage) {
       allImages[i].timesClicked++;
       console.log('Times Clicked: ', allImages[i].timesClicked);
     }
   }
+  createChart();
 };
 
 // create constructor for image objects
-function BusMallImg(name, filepath, imgNum) {
-  this.name = name;
-  this.filepath = filepath;
-  this.imgNum = imgNum;
+function BusImg(path, i) {
+  this.name = path.split('.')[0];
+  this.filepath = 'img/' + path;
+  this.imgNum = i;
   this.timesShown = 0;
   this.timesClicked = 0;
   this.percentClicked = 0;
 };
 
+// create objects for all 19 images: refactored
+function createAllImages() {
+  for (var i = 0; i < filepathArray.length; i++) {
+    allImages.push(new BusImg(filepathArray[i], i));
+  }
+};
 
-// create objects for all 19 images
-// can I make a loop for this?
-var bag = new BusMallImg('Bag', 'img/bag.jpg', 0);
-allImages.push(bag);
+// pull array of strings for myChart
+function generateData() {
+  //clear table data
+  allNames = [];
+  allClicks = [];
+  for (var i = 0; i < allImages.length; i++) {
+    allNames.push(allImages[i].name);
+    allClicks.push(allImages[i].timesClicked);
+  }
+}
 
-var banana = new BusMallImg('Banana', 'img/banana.jpg', 1);
-allImages.push(banana);
+//run functions
+createAllImages();
+generateData();
 
-var bathroom = new BusMallImg('Bathroom', 'img/bathroom.jpg', 2);
-allImages.push(bathroom);
-
-var boots = new BusMallImg('Boots', 'img/boots.jpg', 3);
-allImages.push(boots);
-
-var breakfast = new BusMallImg('Breakfast', 'img/breakfast.jpg', 4);
-allImages.push(breakfast);
-
-var bubblegum = new BusMallImg('Bubble Gum', 'img/bubblegum.jpg', 5);
-allImages.push(bubblegum);
-
-var chair = new BusMallImg('Chair', 'img/chair.jpg', 6);
-allImages.push(chair);
-
-var cthulhu = new BusMallImg('Cthulhu', 'img/cthulhu.jpg', 7);
-allImages.push(cthulhu);
-
-var dogDuck = new BusMallImg('Dog-Duck', 'img/dog-duck.jpg', 8);
-allImages.push(dogDuck);
-
-var dragon = new BusMallImg('Dragon', 'img/dragon.jpg', 9);
-allImages.push(dragon);
-
-var pen = new BusMallImg('Pen', 'img/pen.jpg', 10);
-allImages.push(pen);
-
-var petSweep = new BusMallImg('Pet Sweep', 'img/pet-sweep.jpg', 11);
-allImages.push(petSweep);
-
-var scissors = new BusMallImg('Scissors', 'img/scissors.jpg', 12);
-allImages.push(scissors);
-
-var shark = new BusMallImg('Shark', 'img/shark.jpg', 13);
-allImages.push(shark);
-
-var sweep = new BusMallImg('Sweep', 'img/sweep.png', 14);
-allImages.push(sweep);
-
-var tauntaun = new BusMallImg('Tauntaun', 'img/tauntaun.jpg', 15);
-allImages.push(tauntaun);
-
-var unicorn = new BusMallImg('Unicorn', 'img/unicorn.jpg', 16);
-allImages.push(unicorn);
-
-var usb = new BusMallImg('USB', 'img/usb.gif', 17);
-allImages.push(usb);
-
-var waterCan = new BusMallImg('Water Can', 'img/water-can.jpg', 18);
-allImages.push(waterCan);
-
-var wineGlass = new BusMallImg('Wine Glass', 'img/wine-glass.jpg', 19);
-allImages.push(wineGlass);
-
-// write random generate function, pulling imgNum id per objects
-// Amy helped me with this
+// write random generate function, pulling imgNum id per objects (Amy helped me with this)
 function generateRandom() {
 
   // generate three random numbers
@@ -155,10 +125,10 @@ function createChart() {
   var myChart = new Chart(ctx, {  //eslint-disable-line
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: allNames,
       datasets: [{
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: allClicks,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
